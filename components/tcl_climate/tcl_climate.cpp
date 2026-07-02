f586b74dd155195b1aa28491561fa320124bf9b8
cautermax <63066594+cautermax@users.noreply.github.com>
2026-07-02 18:54:30 +0300

    Update tcl_climate.cpp

--- components/tcl_climate/tcl_climate.cpp
+++ components/tcl_climate/tcl_climate.cpp
@@ -113,11 +113,11 @@ void TCLClimate::control(const climate::ClimateCall &call) {
             get_cmd_resp.data.power = 0x03; // Код увімкнення
             switch (climate_mode) {
                 case climate::CLIMATE_MODE_COOL:     get_cmd_resp.data.mode = 0x01; break;
-                case climate::CLIMATE_MODE_DRY:      get_cmd_resp.data.mode = 0x02; break;
-                case climate::CLIMATE_MODE_FAN_ONLY: get_cmd_resp.data.mode = 0x03; break;
+                case climate::CLIMATE_MODE_DRY:      get_cmd_resp.data.mode = 0x03; break;
+                case climate::CLIMATE_MODE_FAN_ONLY: get_cmd_resp.data.mode = 0x02; break;
                 case climate::CLIMATE_MODE_HEAT:     get_cmd_resp.data.mode = 0x04; break;
                 case climate::CLIMATE_MODE_AUTO:     get_cmd_resp.data.mode = 0x05; break;
-                default:                             get_cmd_resp.data.mode = 0x01; break;
+                default:                             get_cmd_resp.data.mode = 0x02; break;
             }
         }
         should_build_cmd = true;
