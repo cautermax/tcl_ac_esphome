8a8f696949079ed75c5708ac05e9f663ede3b33d
cautermax <63066594+cautermax@users.noreply.github.com>
2026-07-02 14:48:40 +0300

    Update tcl_climate.h

--- components/tcl_climate/tcl_climate.h
+++ components/tcl_climate/tcl_climate.h
@@ -11,40 +11,33 @@ namespace tcl_climate {
 class TCLClimate : public climate::Climate, public uart::UARTDevice, public PollingComponent {
  public:
   TCLClimate(uart::UARTComponent *parent) : UARTDevice(parent) {}
-  TCLClimate() : PollingComponent() {}  // Default constructor for PollingComponent
+  TCLClimate() : PollingComponent() {}
 
   bool is_changed : 1;
-  // Swing position strings
   std::string hswing_pos = "";
   std::string vswing_pos = "";
 
+  // Перероблено під Новий 21-байтний протокол TCL/Ballu
   union get_cmd_resp_t {
     struct {
-      uint8_t header;
-      uint8_t byte_1;
-      uint8_t byte_2;
-      uint8_t type;
-      uint8_t len;
-      uint8_t byte_5;
-      uint8_t byte_6;
+      uint8_t header;     // 0xBB
+      uint8_t byte_1;     // 0x01
+      uint8_t byte_2;     // 0x00
+      uint8_t type;       // 0x03
+      uint8_t len;        // 0x0F
+      uint8_t byte_5;     // 0x01
+      uint8_t byte_6;     // 0x00
       
-      uint8_t mode : 4;
-      uint8_t power : 1;
-      uint8_t disp : 1;
-      uint8_t eco : 1;
-      uint8_t turbo : 1;
+      // Байт 7: Стан та Режим
+      uint8_t mode : 4;   // Молодші 4 біти: 1 - cool, 4 - heat
+      uint8_t power : 4;  // Старші 4 біти: 3 - ON, 2 - OFF
 
-      uint8_t temp : 4;
-      uint8_t fan : 3;
-      uint8_t byte_8_bit_7 : 1;
+      // Байт 8: Температура та Вентилятор
+      uint8_t temp : 4;   // Молодші 4 біти: температура (+16)
+      uint8_t fan : 4;    // Старші 4 біти: швидкість вентилятора
 
       uint8_t byte_9;
-
-      uint8_t byte_10_bit_0_4 : 5;
-      uint8_t hswing : 1;
-      uint8_t vswing : 1;
-      uint8_t byte_10_bit_7 : 1;
-
+      uint8_t byte_10;
       uint8_t byte_11;
       uint8_t byte_12;
       uint8_t byte_13;
@@ -54,64 +47,11 @@ class TCLClimate : public climate::Climate, public uart::UARTDevice, public Poll
       uint8_t byte_17;
       uint8_t byte_18;
       uint8_t byte_19;
-
       uint8_t byte_20;
-      uint8_t byte_21;
-      uint8_t byte_22;
-      uint8_t byte_23;
-      uint8_t byte_24;
-      uint8_t byte_25;
-      uint8_t byte_26;
-      uint8_t byte_27;
-      uint8_t byte_28;
-      uint8_t byte_29;
-
-      uint8_t byte_30;
-      uint8_t byte_31;
-      uint8_t byte_32;
       
-      uint8_t byte_33_bit_0_6 : 7;
-      uint8_t mute : 1;
-
-      uint8_t byte_34;
-      uint8_t byte_35;
-      uint8_t byte_36;
-      uint8_t byte_37;
-      uint8_t byte_38;
-      uint8_t byte_39;
-
-      uint8_t byte_40;
-      uint8_t byte_41;
-      uint8_t byte_42;
-      uint8_t byte_43;
-      uint8_t byte_44;
-      uint8_t byte_45;
-      uint8_t byte_46;
-      uint8_t byte_47;
-      uint8_t byte_48;
-      uint8_t byte_49;
-
-      uint8_t byte_50;
-
-      uint8_t vswing_fix : 3;
-      uint8_t vswing_mv : 2;
-      uint8_t byte_51_bit_5_7 : 3;
-
-      uint8_t hswing_fix : 3;
-      uint8_t hswing_mv : 3;
-      uint8_t byte_52_bit_6_7 : 2;
-
-      uint8_t byte_53;
-      uint8_t byte_54;
-      uint8_t byte_55;
-      uint8_t byte_56;
-      uint8_t byte_57;
-      uint8_t byte_58;
-      uint8_t byte_59;
-
-      uint8_t xor_sum;
+      uint8_t xor_sum;    // Контрольна сума в кінці 21-го байту
     } data;
-    uint8_t raw[61];
+    uint8_t raw[21];      // Тепер довжина пакету відповіді чітко 21 байт
   };
 
   union set_cmd_t {
@@ -204,7 +144,6 @@ class TCLClimate : public climate::Climate, public uart::UARTDevice, public Poll
 
   void set_hswing_pos(const std::string &hswing_pos);
   void set_vswing_pos(const std::string &vswing_pos);
-  // Swing control methods
   void control_vertical_swing(const std::string &swing_mode);
   void control_horizontal_swing(const std::string &swing_mode);
 
